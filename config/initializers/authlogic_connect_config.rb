bc3b2a0f83db93b56303fd9a98776776ead0e29d
Lance Pollard <lancejpollard@gmail.com>
2010-05-12 18:04:42 -0700

    working now with all 3 services

--- config/initializers/authlogic_connect_config.rb
+++ config/initializers/authlogic_connect_config.rb
@@ -1,1 +1,36 @@
-AuthlogicConnect.config = YAML.load_file("config/authlogic.yml")
+# This is how your config file should look.
+# This is my Heroku config file.
+# Heroku recommends setting environment variables on their system
+
+case Rails.env
+when "development"
+  AuthlogicConnect.config = YAML.load_file("config/authlogic.yml")
+when "production"
+  AuthlogicConnect.config = {
+    :services => {
+      :twitter => {
+        :key => ENV["SERVICES_TWITTER_KEY"],
+        :secret => ENV["SERVICES_TWITTER_SECRET"],
+        :label => "Twitter"
+      },
+      :facebook => {
+        :key => ENV["SERVICES_FACEBOOK_KEY"],
+        :secret => ENV["SERVICES_FACEBOOK_SECRET"],
+        :label => "Facebook"
+      },
+      :google => {
+        :key => ENV["SERVICES_GOOGLE_KEY"],
+        :secret => ENV["SERVICES_GOOGLE_SECRET"],
+        :label => "Google"
+      },
+      :yahoo => {
+        :key => ENV["SERVICES_YAHOO_KEY"],
+        :secret => ENV["SERVICES_YAHOO_SECRET"],
+        :label => "Yahoo"
+      },
+      :vimeo => {
+      
+      }
+    }
+  }
+end
